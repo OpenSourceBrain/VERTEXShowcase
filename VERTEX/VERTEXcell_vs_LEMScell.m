42d8d45b4bb0013b6dc56cb7a8b28a45fc87dfc4
RokasSt <rokas.stanislovas.13@ucl.ac.uk>
2015-09-04 22:08:17 +0100

    modified VERTEXcell_vs_LEMScell.m

--- VERTEX/VERTEXcell_vs_LEMScell.m
+++ VERTEX/VERTEXcell_vs_LEMScell.m
@@ -7,11 +7,13 @@ function VERTEXcell_vs_LEMScell(VERTEX_params,Results,filename,cellID_in_pop,pop
 if isstruct(VERTEX_params)==1
     Tissue_params=VERTEX_params.TissueParams;
     Recording_params=VERTEX_params.RecordingSettings;
+    SimulationSettings=VERTEX_params.SimulationSettings;
 end
 
 if iscell(VERTEX_params)==1
     Tissue_params=VERTEX_params{1};
     Recording_params=VERTEX_params{4};
+    SimulationSettings=VERTEX_params{5};
    
     
 end
@@ -21,15 +23,21 @@ if popID~=0
     cellID=cellID_in_pop+1+Tissue_params.groupBoundaryIDArr(popID+1,1);
 end
 index=find(Recording_params.v_m==cellID);
+num_label=0:50:SimulationSettings.simulationTime;
+label_array=cell(1,length(num_label));
+for i=1:length(num_label)
+    label_array{i}=num2str(num_label(i));
+end
 % Visualize results from both VERTEX and LEMS simulations
 x=plot(Results.v_m(index, :), 'LineWidth', 2,'Color','r');
 dat_path=which(sprintf('%s.dat',filename));
 hold on
 load(dat_path);
-y=plot(1000*eval(sprintf('%s(:,1)',filename)),1000*eval(sprintf('%s(:,2)',filename)),'LineWidth',2,'Color','b');
+y=plot(eval(sprintf('%s(:,2)',filename)),'LineWidth',2,'Color','b');
 set(gcf,'color','w');
+set(gca,'XTickLabel',label_array)
 set(gca,'FontSize',16)
 legend([x,y],'VERTEX','LEMS','Location','NorthEastOutside')
-title('Membrane potential of the Adex point neuron in the LEMS and VERTEX simulations', 'FontSize', 16)
+title('Membrane potential of the FitzHughNagumo point neuron in the LEMS and VERTEX simulations', 'FontSize', 16)
 xlabel('Time (ms)', 'FontSize', 16)
 ylabel('Membrane potential (mV)', 'FontSize', 16)
