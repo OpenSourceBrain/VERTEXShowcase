3e7e9a0b7f8c9eb207a31543da9e040b5db8a913
RokasSt <rokas.stanislovas.13@ucl.ac.uk>
2015-09-06 00:55:38 +0100

    annotation to PointNeuronModel_fitzhughnagumocell.m

--- VERTEX/PointNeuronModel_fitzhughnagumocell.m
+++ VERTEX/PointNeuronModel_fitzhughnagumocell.m
@@ -1,13 +1,9 @@
 
+% This is equivalent to LEMS definition of the Fitzhugh Nagumo neuron
+% model, according to FitzHugh R. (1969): Mathematical models of excitation and
+% propagation in nerve. Chapter 1 (pp. 1–85 in H.P. Schwan, ed. Biological Engineering, McGraw–Hill Book Co., N.Y.)
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-%    PLEASE NOTE: This export is still in development
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-               %% the first problem: class name has to include a name of cell type 
-               % not a cell id (previously there was aa fn1_flat)
 classdef PointNeuronModel_fitzhughnagumocell < PointNeuronModel
 
   
@@ -22,7 +18,7 @@ classdef PointNeuronModel_fitzhughnagumocell < PointNeuronModel
   methods
     function NM = PointNeuronModel_fitzhughnagumocell(Neuron, number)
       NM = NM@PointNeuronModel(Neuron, number);
-      %% the second problem: if there is no on start value, set to zero (previously there was Neuron.0.* ...)
+      
       NM.v = Neuron.v0.* ones(number, 1);
       NM.w=Neuron.w0.*ones(number,1);
       NM.spikes = [];
@@ -31,17 +27,11 @@ classdef PointNeuronModel_fitzhughnagumocell < PointNeuronModel
     function [NM] = updateNeurons(NM, IM, N, SM, dt)
       I_syn = PointNeuronModel.sumSynapticCurrents(SM);
       I_input = PointNeuronModel.sumInputCurrents(IM);
-        %the third problem: state variables have to be obtained through NM
-        %constructor 
-        %the fourth problem: parameters have to be obtained through N
-        %arrays
-        % the fifth problem: SEC has no associated value, simple divide by
-        % one (?)
+        
          
          kv=(NM.v-((NM.v)^3)/3 - NM.w + N.I);
          kw=N.phi * (NM.v + N.a - N.b *NM.w);
-        % the fifth problem: second-order Runge Kutta has to be specified
-        % in each cell class:
+        
         k2v_in = NM.v + 0.5 .* dt .* kv;
         k2w_in = NM.w + 0.5 .* dt .* kw;
         
